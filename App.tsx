f7ab1a732d56fbbb6f5c0a4112ebbed3f4d86e9f
nbr41to <nbr.41to@gmail.com>
2021-08-18 22:47:04 +0900

    .

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { NativeBaseProvider, Button } from 'native-base';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <StyledView>
+        <StatusBar style='auto' />
+        <StyledText>Hello world!!</StyledText>
+        <Button onPress={() => alert('Are you ready??')}>PUSH</Button>
+      </StyledView>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const StyledText = styled.Text`
+  color: red;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+const StyledView = styled.View`
+  background-color: skyblue;
+  height: 100%;
+  color: red;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
